18db19b7ec8da9fa8362c6718e02a8fcf86a4496
Jacob W <222jacobw@gmail.com>
2024-08-25 06:44:45 -0500

    Enable ensure installed LSP's

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,7 +8,18 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
-			require("mason-lspconfig").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"basedpyright",
+					"rust_analyzer",
+					"tsserver",
+					"clangd",
+					"emmet_language_server",
+					"cssls",
+					"css_variables",
+				},
+			})
 		end,
 	},
 	{
@@ -17,6 +28,7 @@ return {
 			local lspconfig = require("lspconfig")
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			capabilities.textDocument.completion.completionItem.snippetSupport = true
+			vim.lsp.protocol.make_client_capabilities().workspace.didChangeWatchedFiles.dynamicRegistration = false
 			lspconfig.lua_ls.setup({ capabilities })
 			lspconfig.basedpyright.setup({ capabilities })
 			lspconfig.rust_analyzer.setup({ capabilities })
