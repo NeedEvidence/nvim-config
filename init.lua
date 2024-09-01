07fb05796254e900a21cb8f8f2e6db67af8891ba
Jacob W <222jacobw@gmail.com>
2024-09-01 13:32:01 -0500

    Change tab to two spaces

--- init.lua
+++ init.lua
@@ -22,10 +22,10 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 require("lazy").setup("plugins")
 
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 vim.cmd("set smarttab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
 vim.cmd("colorscheme kanagawa")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
