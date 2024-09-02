febb65315f13fe1df32b1caa38db1a4f8587c6c0
Jacob W <222jacobw@gmail.com>
2024-09-01 21:26:37 -0500

    Fix treesitter syntax error

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 
 	"nvim-treesitter/nvim-treesitter",
-	opts = { 
+	opts = {
 		-- A list of parser names, or "all" (the listed parsers MUST always be installed)
 		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
@@ -10,10 +10,9 @@ return {
 		auto_install = true,
 		highlight = {
 			enable = true,
-
 		},
 		indent = {
 			enable = true,
-		}
-	}
+		},
+	},
 }
