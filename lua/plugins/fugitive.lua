ff4c4fe692d4faae4f3788dae2f7d1a76b9bcbf7
Jacob W <222jacobw@gmail.com>
2024-09-01 21:20:35 -0500

    Improve git shortcuts

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,8 +2,8 @@ return {
 
 	"tpope/vim-fugitive",
 	lazy = false,
-	config=function()
-		vim.keymap.set("n","<leader>ga","<cmd>Git add<cr>")
-		vim.keymap.set("n","<leader>gc","<cmd>Git commit<cr>")
-	end
+	config = function()
+		vim.keymap.set("n", "<leader>ga", "<cmd>Git add .<cr>")
+		vim.keymap.set("n", "<leader>gc", "<cmd>Git commit .<cr>")
+	end,
 }
