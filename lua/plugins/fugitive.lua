c5c34097620c49c298d8d30503108a922c334961
Jacob W <222jacobw@gmail.com>
2024-09-20 16:08:39 -0500

    Fresh start

--- lua/plugins/fugitive.lua
+++
@@ -1,9 +0,0 @@
-return {
-
-	"tpope/vim-fugitive",
-	lazy = false,
-	config = function()
-		vim.keymap.set("n", "<leader>ga", "<cmd>Git add .<cr>")
-		vim.keymap.set("n", "<leader>gc", "<cmd>Git commit .<cr>")
-	end,
-}
--- lua/plugins/toggleterm.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"akinsho/toggleterm.nvim",
-	config = function()
-		require("toggleterm").setup()
-		vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm size=20 direction=horizontal name=terminal<cr>")
-	end,
-}
