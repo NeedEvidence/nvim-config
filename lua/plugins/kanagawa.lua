61b6670d53a32fbc2c9b261d10327d11c4b9360f
Jacob W <222jacobw@gmail.com>
2024-09-21 10:44:13 -0500

    change lsp

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,11 +1,10 @@
 return {
 	"rebelot/kanagawa.nvim",
 	config = function()
-		require'kanagawa'.setup{
+		require("kanagawa").setup({
 			colors = {
-				theme = { all = { ui = { bg_gutter = 'none' }  }}
-			}
-		}
-	end
+				theme = { all = { ui = { bg_gutter = "none" } } },
+			},
+		})
+	end,
 }
-
