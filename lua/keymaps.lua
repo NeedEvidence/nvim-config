eabcd6d024adf3bbea89b04db7bb29edf363472c
Jacob W <222jacobw@gmail.com>
2024-11-18 15:17:08 -0600

    Create floating diagnostic keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -33,7 +33,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
-
+vim.keymap.set('n', '<leader>dd', function()
+  vim.diagnostic.open_float()
+end, { desc = 'Open floating diagnostic' })
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
