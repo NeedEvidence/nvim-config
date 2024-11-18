6f6a5dbf947347d99c73bdc14154c0e11854d27f
Jacob W <222jacobw@gmail.com>
2024-11-18 14:26:47 -0600

    did something regarding completion menu

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -34,7 +34,6 @@ return {
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
-      'hrsh7th/cmp-buffer',
     },
     config = function()
       -- See `:help cmp`
@@ -136,7 +135,6 @@ return {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
-          { name = 'buffer' },
         },
         experimental = {
           ghost_text = true,
