636b1ccf89755039d36519b6dffe7a277347d592
Jacob W <222jacobw@gmail.com>
2025-01-19 12:20:53 -0600

    edits

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -38,8 +38,6 @@ return {
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
-        cpp = { 'clang-format' },
-        c = { 'clang-format' },
       },
     },
   },
