9ce293f679cf3f9affd882d1fcdaaf22599779b2
Jacob W <222jacobw@gmail.com>
2025-02-21 18:03:09 -0600

    lazygit is odd

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,18 +1,23 @@
 return {
   "folke/snacks.nvim",
+  priority = 1000,
   opts = {
     animate = {
+      enabled = true,
     },
     bigfile = {
+      enabled = true,
     },
     terminal = {
-
+      enabled = true,
     },
     lazygit = {
-
+      enabled = true,
     },
-    scroll = {},
-    rename = {},
+    scroll = {
+      enabled = true,
+    },
+    rename = { enabled = true, },
 
   },
 }
