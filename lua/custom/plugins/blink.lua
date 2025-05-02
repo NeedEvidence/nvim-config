432f86e9d19caf0161e0870d82c9c94014f448eb
Jacob W <222jacobw@gmail.com>
2025-05-02 11:02:02 -0500

    Edits

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -3,9 +3,9 @@ return {
     'L3MON4D3/LuaSnip',
     dependencies = 'rafamadriz/friendly-snippets',
     version = "v2.*",
-    config = function() 
+    config = function()
       require('luasnip.loaders.from_vscode').lazy_load()
-      require('luasnip.loaders.from_vscode').lazy_load({ paths = {'~/.config/nvim/luasnippets/'}})
+      require('luasnip.loaders.from_lua').load({ paths = { '~/.config/nvim/luasnippets/' } })
     end
 
 
@@ -14,7 +14,7 @@ return {
     'saghen/blink.cmp',
     lazy = false, -- lazy loading handled internally
     -- optional: provides snippets for the snippet source
-    dependencies = {'L3MON4D3/LuaSnip'},
+    dependencies = { 'L3MON4D3/LuaSnip' },
     enabled = true,
     -- use a release tag to download pre-built binaries
     version = 'v0.*',
@@ -54,13 +54,13 @@ return {
       -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- adjusts spacing to ensure icons are aligned
       ---@diagnostic disable-next-line: missing-fields
+      snippets = { preset = 'luasnip' },
       sources = {
         default = { 'lsp', 'path', 'snippets' },
       },
       cmdline = {
         enabled = false,
       },
-      snippets = { preset = 'luasnip' },
       completion = {
         documentation = {
           auto_show = true,
---
+++ lua/custom/plugins/maple.lua
@@ -0,0 +1,28 @@
+return {
+  'Forest-nvim/maple.nvim',
+  config = function()
+    require('maple').setup({
+      width = 0.6,      -- Width of the popup (ratio of the editor width)
+      height = 0.6,     -- Height of the popup (ratio of the editor height)
+      border = 'rounded', -- Border style ('none', 'single', 'double', 'rounded', etc.)
+      title = ' notes ',
+      title_pos = 'center',
+      winblend = 0,     -- Window transparency (0-100)
+      show_legend = true, -- Whether to show keybind legend in the UI
+
+      -- Storage
+      storage_path = vim.fn.stdpath('data') .. '/maple',
+
+      -- Notes management
+      notes_mode = "project",          -- "global" or "project"
+      use_project_specific_notes = true, -- Store notes by project
+
+      -- Keymaps (set to nil to disable)
+      keymaps = {
+        toggle = '<leader>mn', -- Key to toggle Maple
+        close = 'q',          -- Key to close the window
+        switch_mode = 'm',    -- Key to switch between global and project view
+      }
+    })
+  end
+}
